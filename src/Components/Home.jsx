7d0a1548d55d551a9836dce3241968cdf500173e
GITA <jgitanjali592@gmail.com>
2024-04-23 22:28:02 +0530

    this code for horizentalCards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,14 @@ import Sidenav from '../templates/Sidenav';
 import Topnav from '../templates/Topnav';
 import axios from '../utils/axios';
 import Header from '../templates/Header';
+import HorizentalCards from '../templates/HorizentalCards';
 
 const Home = () => {
   document.title = 'Homepage';
 
   const [wallpaper, setwallpaper] = useState(null);
+  const [trending, setTrending] = useState(null);
+
   const getHeaderWallpaper = async () => {
     try {
       const { data } = await axios.get(`/trending/all/day`);
@@ -18,18 +21,34 @@ const Home = () => {
     }
   };
 
+
+  const getTrending = async () => {
+    try {
+      const { data } = await axios.get(`/trending/all/day`);
+      setTrending(data.results);
+      // console.log(randomWallpaper);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
+
   // console.log(wallpaper);
 
   useEffect(() => {
     !wallpaper && getHeaderWallpaper();
-  }, [wallpaper]);
+    !trending && getTrending();
+  }, []);
 
-  return wallpaper ? (
+  console.log(trending);
+
+  return wallpaper && trending  ? (
     <>
       <Sidenav />
-      <div className="w-[80%] h-full ">
+      <div className="w-[80%] h-full overflow-auto  overflow-x-hidden">
         <Topnav />
         <Header data={wallpaper} />
+        <HorizentalCards data={trending}/>
+
       </div>
     </>
   ) : (
