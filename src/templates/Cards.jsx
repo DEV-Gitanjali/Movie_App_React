bc237e04ff061766c61b2bac17345b653fdc140b
GITA <jgitanjali592@gmail.com>
2024-04-24 13:42:20 +0530

    this code for trending data cards

---
+++ src/templates/Cards.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Cards = ({ data, title }) => {
+ 
+    if (!Array.isArray(data)) {
+        return <div>Data is not an array.</div>;
+      }
+    
+      return (
+        <div className='flex flex-wrap w-[full] ml-10'>
+          {data.map((c, i) => (
+            <Link className='w-[25vh] mr-[5%] mb-[5%]' key={i}>
+              <img
+                className=' shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover'
+                src={`https://image.tmdb.org/t/p/original/${c.backdrop_path || c.profile_path}`}
+                alt=''
+              />
+
+              <h1 className='text-2xl text-zinc-300 mt-2 font-semibold'>
+              {c.name || c.title || c.original_name || c.original_title}
+
+              </h1>
+
+
+            </Link>
+          ))}
+        </div>
+      );
+    };
+export default Cards;
